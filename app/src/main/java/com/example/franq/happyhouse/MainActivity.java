60966539a20d6a58dc735286299776912ea38bf1
Franqu <maciejedrzejko@gmail.com>
2019-06-19 01:57:54 +0200

    1906

--- app/src/main/java/com/example/franq/happyhouse/MainActivity.java
+++ app/src/main/java/com/example/franq/happyhouse/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.MenuItem;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+//private SQLiteDao sqLiteDao = new SQLiteDao();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         });*/
     }
 
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -51,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+*/
 
     public void openMemory(View view)
     {
